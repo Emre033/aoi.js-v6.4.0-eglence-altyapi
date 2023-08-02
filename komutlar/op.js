f0678867c67313ae11d5125638bf75133222763d
Emre <118940184+Emre033@users.noreply.github.com>
2023-08-02 14:06:31 +0300

    Update op.js

--- komutlar/op.js
+++ komutlar/op.js
@@ -1,9 +1,9 @@
 module.exports = {
-    name:"tokat",
+    name:"op",
     code:`
-    $description[1;<@$authorID> Kullanıcısı <@$mentioned[1]> kullanıcısına tokat attı!]
+    $description[1;<@$authorID> Kullanıcısı <@$mentioned[1]> kullanıcısına optu!]
     $image[$jsonRequest[https://elixirapi.xyz/randomGif?filtre=öp;gifUrl]]
-    $onlyIf[$mentioned[1]!=$authorID;Kendine Tokat Atamazsin!]
+    $onlyIf[$mentioned[1]!=$authorID;Kendini opemezsin!]
     $onlyIf[$mentioned[1]!=undefined;Bir kullanıcı Belirt!]
     
     `
